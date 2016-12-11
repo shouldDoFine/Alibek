b5ad9c98d196e467cc8cf54fbe469279a06a55c2
KasperNisan <kaltaker@yandex.ru>
2016-12-11 15:59:37 +0300

    feature2 first commit

---
+++ src/main/java/alibek/ChatServer.java
@@ -0,0 +1,76 @@
+package alibek;
+
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.*;
+
+
+public class ChatServer {
+    ArrayList clientOutputStreams;
+    private ServerSocket serverSocket;
+    public Map<String, User> userHashMap;
+
+
+    public ChatServer() {
+        try {
+            this.serverSocket = new ServerSocket(5000);
+            userHashMap = new HashMap<String, User>();
+
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void go() {
+        clientOutputStreams = new ArrayList();
+        try {
+            while (true) {
+                Socket  clientSocket = serverSocket.accept();
+                ClientHandler clientHandler = new ClientHandler(clientSocket);
+                clientHandler.setChat(this);
+                Thread t = new Thread(clientHandler);
+                t.start();
+                System.out.println("got a connection");
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void addUserToMap(User user, PrintWriter writer) {
+        userHashMap.put(user.getName(), user);
+
+    }
+
+    public void tellEveryone(String message, PrintWriter writer) {
+        for ( Map.Entry<String, User> entry : userHashMap.entrySet()) {
+            String name = entry.getKey();
+            User user = entry.getValue();
+            if(!user.isMe(name)) {
+                writer.println(message);
+                writer.flush();
+            }
+            else continue;
+        }
+    }
+
+
+    public static void main(String[] args) {
+        new ChatServer().go();
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ src/main/java/alibek/User.java
@@ -0,0 +1,33 @@
+package alibek;
+
+import java.util.*;
+
+
+public class User {
+    String name;
+    User(String name) throws Exception {
+        validateName(name);
+        this.setName(name);
+    }
+    User() {
+
+    }
+    public String getName() {
+        return this.name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public boolean isMe(String name) {
+        return this.name.equals(name);
+    }
+    public void validateName(String name) throws Exception {
+        if( Character.isDigit(name.charAt(0)) ) {
+            throw new Exception();
+        }
+    }
+
+
+
+
+}
